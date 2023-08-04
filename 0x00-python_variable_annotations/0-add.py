908c4edbaa027dd7deb3656658a67b89ed0284e4
Toby16 <bolajibaasit14@gmail.com>
2023-08-03 20:05:57 -0400

    Basic annotations - add | Report: Task Done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function to add two floating-point numbers
+    """
+    return a + b
+
+
+if __name__ == "__main__":
+    print(add(1.11, 2.22) == 1.11 + 2.22)
+    print(add.__annotations__)
