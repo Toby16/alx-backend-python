95cf29c508cc897ff5bb28bbcfeb29e1dbb7185e
Toby16 <bolajibaasit14@gmail.com>
2023-08-03 22:09:09 -0400

    More involved type annotations | Report: Advanced Task Done

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+More involved type annotations
+"""
+from typing import Mapping, Any, TypeVar, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping[Any, T], key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Retrieves a value from a dict using a given key.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+
+
+if __name__ == "__main__":
+    annotations = safely_get_value.__annotations__
+
+    print("Here's what the mappings should look like")
+    for k, v in annotations.items():
+        print(("{}: {}".format(k, v)))
