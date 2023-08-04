b34e7793ec4e2617aa7724235c28c123c5ba0c24
Toby16 <bolajibaasit14@gmail.com>
2023-08-03 20:09:36 -0400

    Basic annotations - concat | Report: Task Done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function to concat two strings
+    """
+    return str1 + str2
+
+
+if __name__ == "__main__":
+    str1 = "egg"
+    str2 = "shell"
+
+    print(concat(str1, str2) == "{}{}".format(str1, str2))
+    print(concat.__annotations__)
