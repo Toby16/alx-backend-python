0ca379bbda697f82dfd03511463993f725b919be
Toby16 <bolajibaasit14@gmail.com>
2023-08-27 22:38:55 -0400

    Parameterize a unit test | Report: Task Done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,9 @@ Parameterize a unit test
 import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
-from utils import access_nested_map
+from unittest.mock import Mock
+from utils import (access_nested_map,
+                   get_json)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,7 +29,6 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
-
     @parameterized.expand([
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
@@ -43,6 +44,28 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Tests the 'get_json' function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        """
+        Tests get_json's output
+        """
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
