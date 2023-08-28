0510a749181bbc57370fdd522ff4308d8cc28a15
Toby16 <bolajibaasit14@gmail.com>
2023-08-27 21:51:46 -0400

    Parameterize a unit test | Report: Task Done

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Parameterize a unit test
+"""
+import unittest
+import parameterized
+from typing import Dict, Tuple, Union
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Tests the 'access_nested_map' function
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self,
+                               nested_map: Dict,
+                               path: Tuple[str],
+                               expected_result: Union[Dict, int]) -> None:
+        """
+        Tests `access_nested_map`'s output
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
