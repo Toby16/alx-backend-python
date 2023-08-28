1712ac542927326bf0fed0efc25d32359b568e8f
Toby16 <bolajibaasit14@gmail.com>
2023-08-27 22:43:33 -0400

    Parameterize and patch | Report: Task Done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,13 +2,15 @@
 """
 Parameterize a unit test
 Mock HTTP calls
+Parameterize and patch
 """
 import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
 from utils import (access_nested_map,
-                   get_json)
+                   get_json,
+                   Memoize)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -67,6 +69,32 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**attrs)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Tests the memoize function
+    """
+    def test_memoize(self) -> None:
+        """
+        Tests memoize's output
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 
 
 if __name__ == "__main__":
