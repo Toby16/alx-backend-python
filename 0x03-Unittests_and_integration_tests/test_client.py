9cf4bd644c37b2318ae0af29534ab7771b6ba40b
Toby16 <bolajibaasit14@gmail.com>
2023-08-28 21:14:27 -0400

    Parameterize and patch as decorators | Report: Task Done

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,4 +3,28 @@
 Parameterize and patch as decorators
 """
 
+import unittest
 from client import GithubOrgClient
+from typing import Dict
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Tests the 'GithubOrgClient' class
+    """
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch("client.get_json",)
+    def test_org(self, org: str,
+                 resp: Dict, mocked_fxn: MagicMock) -> None:
+        """
+        Tests the 'org' method
+        """
+        mocked_fxn.return_value = MagicMock(return_value=resp)
+        gh_org_client = GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), resp)
+        mocked_fxn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
